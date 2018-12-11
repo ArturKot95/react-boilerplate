7f7112758795f299f032a850a593c285f06df810
Artur Kot <arturkot95@gmail.com>
2018-12-11 12:02:11 +0100

    Basic React-Router boilerplate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App.js';
+import AppRouter from './components/AppRouter.js';
 
 ReactDOM.render(
-  <App user={'Artur'} />,
+  <AppRouter />,
   document.getElementById('root')
 );
